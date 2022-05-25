bc77ef9dbf8f14be71aecdf69076af8e38fc4157
Lalu Tata Linggiang Raisahi 21103079 <101233671+atang-28@users.noreply.github.com>
2022-05-25 17:27:32 +0800

    Delete progwebAtang directory

--- progwebAtang/server/dtmhs.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once("dbkoneksi2.php");
-	$sql = "SELECT NIM, NAMA, JURUSAN, JK, TGLLAHIR FROM mhs";
-	$hsl = mysqli_query($cnn, $sql);
-	$output = mysqli_fetch_all($hsl);
-	echo json_encode($output);
--- progwebAtang/server/konfigurasi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //mendefinisikan komponen koneksi DBMS mysql
-    define("DBHOST","127.0.0.1");
-    define("DBUSER","root");
-    define("DBPASCODE","");
-    define("DBNAME","mahasiswa");
-    define("DBPORT","3306");
